7d49a7ec18a7c072eff4594cda8af90f32b660f1
Md. Rashedul Hasan <76987245+mdrashedulhasan1@users.noreply.github.com>
2022-12-04 09:58:27 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+//comment
+// Single Line Comment
+/*
+   Multi-Line Comment
+*/
+
+//String
+// var love = 'I Love to Learn Coding';
+// console.log(love.toLowerCase());
+// console.log(love.toUpperCase());
+// console.log(love.indexOf("earn"));
+// console.log(love.indexOf("sdfv"));
+// console.log(love.split("Learn"));
+
+//Integer,float,parseInt,parseFloat type conversion
+// var mangoPrice1 = 10;
+// var mangoPrice2 = '20.75';
+// mangoPrice1 = ""+mangoPrice1;
+// mangoPrice1 = mangoPrice1.toString(mangoPrice1);
+// console.log(typeof mangoPrice1);
+// mangoPrice2 = parseInt(mangoPrice2);
+// mangoPrice2 = + mangoPrice2;
+// mangoPrice2 = parseFloat(mangoPrice2);
+// console.log( mangoPrice2);
+
+
+//Increment, Decrement
+// var num = 5;
+// num++;
+// num = num+1;
+// num--;
+// num = num-1;
+// console.log(num);
+
+//Concatenate Strings
+var firstName = "Rashedul "
+var lastName = "Hasan";
+// var fullName = firstName +" "+ lastName;
+var fullName = firstName.concat(lastName);
+console.log(fullName);
+
